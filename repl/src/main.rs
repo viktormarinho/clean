4d9e2005ccffc0db322ffbcb1c6f0e603612e23e
viktormarinho <viktormpcs@gmail.com>
2023-02-14 06:25:27 -0300

    start repo

---
+++ repl/src/main.rs
@@ -0,0 +1,30 @@
+use rustyline::error::ReadlineError;
+use rustyline::{Editor, Result};
+
+fn main() -> Result<()> {
+    let mut rl = Editor::<()>::new()?;
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());    
+                println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+    rl.save_history("history.txt")
+}
