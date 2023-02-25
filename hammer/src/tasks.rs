20fcfa1cdadb7c56605bdd04f2f3af5a1519a065
viktormarinho <viktormpcs@gmail.com>
2023-02-25 12:51:05 -0300

    .env command line arg overriding

--- hammer/src/tasks.rs
+++ hammer/src/tasks.rs
@@ -1,11 +1,12 @@
+use colored::Colorize;
 use std::collections::HashMap;
-use std::process::{Command, Stdio};
+use std::io::Write;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
-use std::{io, env};
-use colored::Colorize;
-use std::io::Write;
+use std::process::{Command, Stdio};
+use std::{env, io};
 
+use crate::errors::BeautifulErrors;
 use crate::npm_process::NpmProcessContext;
 
 fn handle_process_stdout(process_name: &String, msg: &str) {
@@ -17,29 +18,52 @@ fn handle_process_stdout(process_name: &String, msg: &str) {
 
 pub fn start_npm_process(context: NpmProcessContext) {
     let path = Path::new(&context.dir);
-    let root_env: HashMap<String, String> = env::vars().collect();
+    let mut root_env: HashMap<String, String> = env::vars().collect();
+
+    if let Some(envs) = context.args.env {
+        envs.iter().for_each(|var| {
+            let key_value: Vec<&str> = var.split(":").collect();
+            let key = key_value.get(0).expect_or_err(
+                &format!("Invalid environment variable provided: {} - Usage: hammer dev -e node_env:development", var)
+            );
+
+            let val = key_value.get(1).expect_or_err(
+                &format!("Invalid environment variable provided: {} - Usage: hammer dev -e node_env:development", var)
+            );
+
+            root_env.insert(key.to_string(), val.to_string());
+        });
+    }
     let mut child = Command::new("npm")
-            .current_dir(path)
-            .envs(&root_env)
-            .arg("run")
-            .arg(context.script)
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect(&format!("Could not start child process on directory {}", path.display()));
-    
+        .current_dir(path)
+        .envs(&root_env)
+        .arg("run")
+        .arg(context.args.script)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect(&format!(
+            "Could not start child process on directory {}",
+            path.display()
+        ));
+
     tokio::spawn(async move {
-        let mut f = BufReader::new(child.stdout.take().expect("Could not retrieve child std output"));
+        let mut f = BufReader::new(
+            child
+                .stdout
+                .take()
+                .expect("Could not retrieve child std output"),
+        );
         loop {
             let mut buf = String::new();
             match f.read_line(&mut buf) {
                 Ok(_) => {
                     handle_process_stdout(&context.name, buf.as_str());
-                },
-                Err(e) => println!("child err: {:?}", e)
+                }
+                Err(e) => println!("child err: {:?}", e),
             }
             if let Ok(status) = child.try_wait() {
                 if let Some(_status) = status {
-                    break
+                    break;
                 }
             }
         }
